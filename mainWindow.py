a2419b0e92788d8622de866d11db7a94a073787c
clarkakila <clarkakila@gmail.com>
2017-10-14 21:46:11 +0000

    Main Window update

--- mainWindow.py
+++ mainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainWindow2.ui'
+# Form implementation generated from reading ui file 'mainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.5
 #
@@ -9,19 +9,19 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import QtQuickWidgets
 
-
+from PyQt5.QtCore import QUrl
 
 import sys
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
-
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
-        
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(480, 320)
+        MainWindow.setMinimumSize(QtCore.QSize(480, 320))
+        MainWindow.setMaximumSize(QtCore.QSize(480, 320))
         MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -34,6 +34,13 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.gridLayout_3 = QtWidgets.QGridLayout()
         self.gridLayout_3.setObjectName("gridLayout_3")
         self.splitscreen_button = QtWidgets.QPushButton(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic UI")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.splitscreen_button.setFont(font)
         self.splitscreen_button.setStyleSheet("color: rgb(255, 255, 255);\n"
 "background-color: rgb(0, 0, 0);")
         self.splitscreen_button.setObjectName("splitscreen_button")
@@ -41,14 +48,27 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.quickWidget_3 = QtQuickWidgets.QQuickWidget(self.centralwidget)
         self.quickWidget_3.setObjectName("quickWidget_3")
         self.gridLayout_3.addWidget(self.quickWidget_3, 0, 1, 1, 1)
+
+        self.quickWidget_3.setSource(QUrl('navigationAndInformation.qml')) #add qml to quickwidget
+
         self.horizontalLayout_5.addLayout(self.gridLayout_3)
         self.verticalLayout_2.addLayout(self.horizontalLayout_5)
         self.gridLayout_6 = QtWidgets.QGridLayout()
         self.gridLayout_6.setObjectName("gridLayout_6")
-        self.mainwindow = QtQuickWidgets.QQuickWidget(self.centralwidget)
-        self.mainwindow.setObjectName("mainwindow")
-        self.gridLayout_6.addWidget(self.mainwindow, 0, 0, 1, 1)
+        self.quickWidget_2 = QtQuickWidgets.QQuickWidget(self.centralwidget)
+        self.quickWidget_2.setObjectName("quickWidget_2")
+        self.gridLayout_6.addWidget(self.quickWidget_2, 0, 0, 1, 1)
+
+        self.quickWidget_2.setSource(QUrl('offline.qml')) #add qml to quickwidget
+
         self.offline_map_button = QtWidgets.QPushButton(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic UI")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.offline_map_button.setFont(font)
         self.offline_map_button.setStyleSheet("color: rgb(255, 255, 255);\n"
 "background-color: rgb(0, 0, 0);")
         self.offline_map_button.setObjectName("offline_map_button")
@@ -64,7 +84,17 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.quickWidget_4 = QtQuickWidgets.QQuickWidget(self.centralwidget)
         self.quickWidget_4.setObjectName("quickWidget_4")
         self.gridLayout_2.addWidget(self.quickWidget_4, 1, 0, 1, 1)
+        
+        self.quickWidget_4.setSource(QUrl('information.qml')) #add qml to quickwidget
+
         self.information_button = QtWidgets.QPushButton(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic UI")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.information_button.setFont(font)
         self.information_button.setStyleSheet("background-color: rgb(0, 0, 0);\n"
 "color: rgb(255, 255, 255);")
         self.information_button.setObjectName("information_button")
@@ -77,9 +107,20 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.quickWidget.setStyleSheet("")
         self.quickWidget.setObjectName("quickWidget")
         self.gridLayout_7.addWidget(self.quickWidget, 0, 0, 1, 1)
+
+        self.quickWidget.setSource(QUrl('online.qml')) #add qml to quickwidget
+
         self.online_map_button = QtWidgets.QPushButton(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic UI")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.online_map_button.setFont(font)
         self.online_map_button.setStyleSheet("color: rgb(255, 255, 255);\n"
-"background-color: rgb(0, 0, 0);")
+"background-color: rgb(0, 0, 0);\n"
+"")
         self.online_map_button.setObjectName("online_map_button")
         self.gridLayout_7.addWidget(self.online_map_button, 1, 0, 1, 1)
         self.verticalLayout_3.addLayout(self.gridLayout_7)
@@ -92,12 +133,10 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.splitscreen_button.setText(_translate("MainWindow", "Navigation/Car Information "))
-        self.offline_map_button.setText(_translate("MainWindow", "Offline Maps"))
-        self.information_button.setText(_translate("MainWindow", "Car Information"))
-        self.online_map_button.setText(_translate("MainWindow", "Online Maps"))
-
-
+        self.splitscreen_button.setText(_translate("MainWindow", "Push"))
+        self.offline_map_button.setText(_translate("MainWindow", "Push"))
+        self.information_button.setText(_translate("MainWindow", "Push"))
+        self.online_map_button.setText(_translate("MainWindow", "Push"))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
